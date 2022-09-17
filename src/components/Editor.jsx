9aa78c9dbb82e37f5da1d3737da07a4d5a0892e1
Hardik5862 <67375963+Hardik5862@users.noreply.github.com>
2022-09-17 22:18:00 +0530

    saving editor data

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,5 @@
 import EditorJS from "@editorjs/editorjs";
+import { useEffect, useRef } from "react";
 
 import { EDITOR_JS_TOOLS } from "../configs/editor_config";
 
@@ -6,20 +7,41 @@ import styles from "./Editor.module.css";
 
 const Editor = () => {
   const holder = "editor";
+  const editorInstance = useRef(null);
 
   const createEditor = (node) => {
-    let editor;
     if (node) {
-      editor = new EditorJS({
+      let editor = new EditorJS({
         data: { blocks: [] },
         holder,
         tools: EDITOR_JS_TOOLS,
       });
+      editorInstance.current = editor;
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (editorInstance.current && editorInstance.current.destroy) {
+        editorInstance.current.destroy();
+      }
+    };
+  });
+
+  const handleSave = async () => {
+    if (editorInstance.current && editorInstance.current.save) {
+      const data = await editorInstance.current.save();
+      console.log(data);
     }
   };
 
   return (
-    <div className={styles.container} id={holder} ref={createEditor}></div>
+    <>
+      <div>
+        <button onClick={handleSave}>Save</button>
+      </div>
+      <div className={styles.container} id={holder} ref={createEditor}></div>
+    </>
   );
 };
 export default Editor;
